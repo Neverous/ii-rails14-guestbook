22186a207cb841556dacb728be812ed2fa2af5dc
Maciej Szeptuch (Neverous) <neverous@neverous.info>
2014-10-16 19:00:03 +0200

    added simple validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, :body, :author, presence: true
+    validates :title, length: { in: 4..256 }
+    validates :author, length: { in: 4..64 }
 end
